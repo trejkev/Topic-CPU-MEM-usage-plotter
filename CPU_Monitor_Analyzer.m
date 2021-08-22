69e4cbd8637b98aa315085f8be199d023de92d87
Kevin Trejos <trejkev@gmail.com>
2021-08-21 20:48:25 -0600

    Added header and automatic files removal

--- CPU_Monitor_Analyzer.m
+++ CPU_Monitor_Analyzer.m
@@ -1,8 +1,39 @@
-if exist('Statistics.png', 'file') == 2
-    delete('Statistics.png')
+%----------------------------------------------%
+% Universidad de Costa Rica
+% Engineering Faculty
+% Electrical Engineering School
+% Graduation project: 
+% SLAM Algorithms comparison 
+% using ROS nodes
+%
+% Created by: Kevin Trejos Vargas
+% email: kevin.trejosvargas@ucr.ac.cr
+%
+% Description: This script takes a bag file
+%     containing SLAM algorithm CPU and MEM usage 
+%     messages, then plots and calculates some
+%     relevant statistics about them.
+%
+%
+% Instructions for use:
+%     1. Modify the bagFilePath to point to your rosbag.
+%     2. Modify the cpuMonitorTopic and memMonitorTopic
+%        to point to your CPU and MEM tracking topics.
+%
+% Notes: 
+%     1. CPU and MEM usage are taken by using the following repo:
+%           https://github.com/alspitz/cpu_monitor.git
+%----------------------------------------------%
+
+%% Remove old analysis files
+
+if exist('CPU_MEM_Statistics.png', 'file') == 2
+    delete('CPU_MEM_Statistics.png')
 end
 
-bagFilePath     = '~/.ros/KartoSLAM__2021-06-07-19-36-26.bag';
+clear
+
+bagFilePath     = '~/Documents/Pruebas_SLAM/Full Factorial/Tratamiento_9/KartoSLAM_FullFactorial_Tratamiento9_2021-08-21-08-32-17.bag';
 cpuMonitorTopic = '/cpu_monitor/slam_karto/cpu';
 memMonitorTopic = '/cpu_monitor/slam_karto/mem';
 
@@ -14,8 +45,8 @@ SLAM_MEM_Topic = select(bagselect, 'Topic', memMonitorTopic);
 CPU_messages = readMessages(SLAM_CPU_Topic, 'DataFormat','struct');
 MEM_messages = readMessages(SLAM_MEM_Topic, 'DataFormat','struct');
 
-CPU_Data = cellfun(@(m) double(m.Data), CPU_messages);
-MEM_Data = cellfun(@(m) double(m.Data), MEM_messages);
+CPU_Data    = cellfun(@(m) double(m.Data), CPU_messages);
+MEM_Data    = cellfun(@(m) double(m.Data), MEM_messages);
 MEM_Data_MB = MEM_Data/(1024*1024);                                        % RAM Memory usage in MB
 
 % Generate a figure with all the relevant statistics
@@ -45,23 +76,27 @@ text(0,0.4 ,"Max: "                    ); axis off
 text(0.4,0.4, string(max(CPU_Data))    ); axis off
 text(0,0.3 ,"Mode: "                   ); axis off
 text(0.4,0.3, string(mode(CPU_Data))   ); axis off
-text(0,0.26,"\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_"  ); axis off
-text(0,0.25,"\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_"  ); axis off
+text(0,0.2 ,"Median: "                 ); axis off
+text(0.4,0.2, string(median(CPU_Data)) ); axis off
+text(0,0.16,"\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_"  ); axis off
+text(0,0.15,"\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_"  ); axis off
 subplot(2,2,4)
 text(0,0.96,"\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_"  ); axis off
 text(0,0.95,"\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_"  ); axis off
-text(0,0.8 ,"Mean: "                    ); axis off
-text(0.4,0.8, string(mean(MEM_Data_MB)) ); axis off
-text(0,0.7 ,"Samples: "                 ); axis off
-text(0.4,0.7, string(length(MEM_Data))  ); axis off
-text(0,0.6 ,"Stdev: "                   ); axis off
-text(0.4,0.6, string(std(MEM_Data_MB))  ); axis off
-text(0,0.5 ,"Min: "                     ); axis off
-text(0.4,0.5, string(min(MEM_Data_MB))  ); axis off
-text(0,0.4 ,"Max: "                     ); axis off
-text(0.4,0.4, string(max(MEM_Data_MB))  ); axis off
-text(0,0.3 ,"Mode: "                    ); axis off
-text(0.4,0.3, string(mode(MEM_Data_MB)) ); axis off
-text(0,0.26,"\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_"  ); axis off
-text(0,0.25,"\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_"  ); axis off
-saveas(f,'Statistics','png')
+text(0,0.8 ,"Mean: "                     ); axis off
+text(0.4,0.8, string(mean(MEM_Data_MB))  ); axis off
+text(0,0.7 ,"Samples: "                  ); axis off
+text(0.4,0.7, string(length(MEM_Data))   ); axis off
+text(0,0.6 ,"Stdev: "                    ); axis off
+text(0.4,0.6, string(std(MEM_Data_MB))   ); axis off
+text(0,0.5 ,"Min: "                      ); axis off
+text(0.4,0.5, string(min(MEM_Data_MB))   ); axis off
+text(0,0.4 ,"Max: "                      ); axis off
+text(0.4,0.4, string(max(MEM_Data_MB))   ); axis off
+text(0,0.3 ,"Mode: "                     ); axis off
+text(0.4,0.3, string(mode(MEM_Data_MB))  ); axis off
+text(0,0.2 ,"Median: "                   ); axis off
+text(0.4,0.2, string(median(MEM_Data_MB))); axis off
+text(0,0.16,"\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_"  ); axis off
+text(0,0.15,"\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_\_"  ); axis off
+saveas(f,'CPU_MEM_Statistics','png')
